7d2db9e6b2a67905184e9e1d8a3a31ed41d88e38
Fran <frangomez_96@hotmail.com>
2023-11-09 15:15:14 +0100

    Include login functions

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,8 @@
 
 import plotly.express as px
 import random
-
+import streamlit as st
+import hmac
 
 DIRECTIONS_EMOJI = {
     "North": "⬆️",
@@ -53,8 +54,8 @@ def show_country(df_country):
     )
     return fig
 
+
 def show_country_palo(df_country,name_df,random_colors):
-    
 
     fig = px.treemap(df_country, 
                     path=['variable'],  # Columna que define la jerarquía
@@ -63,15 +64,50 @@ def show_country_palo(df_country,name_df,random_colors):
                     color_discrete_sequence =random_colors,  # Mapeo de colores
                      title=  str(round(df_country['value'].sum()/1e9,2)) +f'B {name_df}' if df_country['value'].sum()>1e9 else str(round(df_country['value'].sum()/1e6,2)) +f'M {name_df}'if df_country['value'].sum()>1e6 else str(round(df_country['value'].sum()/1e3,2)) +f'mil {name_df}'if df_country['value'].sum()>1e3 else str(round(df_country['value'].sum(),2)) +f' {name_df}' ,
                     # labels={'variable': 'Categoría', 'value': 'Valor'}
-                    )
-
+    )
 
     fig.update_layout(
         width=600,  # Ancho de la figura en píxeles
         height=500  # Alto de la figura en píxeles
     )
     return fig
-    
+
+
 def random_color():
     
     return f'rgb({random.randint(0, 255)}, {random.randint(0, 255)}, {random.randint(0, 255)})'
+
+
+def check_password():
+    """Returns `True` if the user had a correct password."""
+
+    def login_form():
+        """Form with widgets to collect user information"""
+        with st.form("Credentials"):
+            st.text_input("Username", key="username")
+            st.text_input("Password", type="password", key="password")
+            st.form_submit_button("Log in", on_click=password_entered)
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["username"] in st.secrets[
+            "passwords"
+        ] and hmac.compare_digest(
+            st.session_state["password"],
+            st.secrets.passwords[st.session_state["username"]],
+        ):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the username or password.
+            del st.session_state["username"]
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the username + password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show inputs for username + password.
+    login_form()
+    if "password_correct" in st.session_state:
+        st.error("😕 User not known or password incorrect")
+    return False
